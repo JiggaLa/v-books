ef50d65280ea3f189fb5203607947fbd97d05734
JiggaLa <la.japh@gmail.com>
2022-01-20 23:11:32 +0200

    feat: add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="w-full mt-[140px] mb-10 flex justify-center items-center">
+    <img
+      src="~assets/img/versa_badge.png"
+      loading="lazy"
+      alt="v-agency-batch"
+      class="w-[120px]"
+    />
+  </footer>
+</template>
